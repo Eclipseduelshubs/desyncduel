ff2124a1ba6059db4c2ab5087cd7ca03ba45c5ee
Eclipseduelshubs <sabtrading99mw@gmail.com>
2026-04-14 19:32:18 +0200

    Update duel.lua to disable script functionality

--- duel.lua
+++ duel.lua
@@ -1,2 +1,4 @@
 -- Script disabled by the owner
-game.Players.LocalPlayer:Kick("Script got disabled by the owner.")
+if not script_key or script_key == "" then
+    game.Players.LocalPlayer:Kick("Script got disabled by the owner.")
+end
